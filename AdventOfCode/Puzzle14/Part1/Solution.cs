827073edbe93671ab068f134b8a9aa8c2bdd2308
Ravinder Singh <rsingh@ravi-singh.com>
2020-12-29 22:39:34 +0000

    Puzzle 14 Part 2.

---
+++ AdventOfCode/Puzzle14/Part1/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Puzzle14.Part1
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            
+        }
+    }
+}

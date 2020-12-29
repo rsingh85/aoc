acdaf282c621f0016a6c139bae6f9f5b6475c434
Ravinder Singh <rsingh@ravi-singh.com>
2020-12-29 21:53:52 +0000

    Puzzle 2, code and cleanup.

---
+++ AdventOfCode/Puzzle2/Part1/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Puzzle2.Part1
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var policyAndPasswords = File.ReadAllLines(@"Puzzle2\Part1\Input.txt");
+            var validPasswordCount = 0;
+
+            foreach (var policyAndPassword in policyAndPasswords)
+            {
+                var match = Regex.Match(policyAndPassword, @"^(\d+)\-(\d+)\s([a-z]):\s([a-z]+)$");
+
+                if (!match.Success)
+                {
+                    continue;
+                }
+
+                var minLetterOccurrence = int.Parse(match.Groups[1].Value);
+                var maxLetterOccurrence = int.Parse(match.Groups[2].Value);
+                var letter = match.Groups[3].Value;
+                var password = match.Groups[4].Value;
+                var letterOccurrences = Regex.Matches(password, letter).Count;
+
+                if (letterOccurrences >= minLetterOccurrence
+                    && letterOccurrences <= maxLetterOccurrence)
+                {
+                    validPasswordCount++;
+                }
+            }
+            Console.WriteLine(validPasswordCount);
+        }
+    }
+}

d2d4073eaf1dc343b6c25db175e22e7996343df0
Ravinder Singh <rsingh@ravi-singh.com>
2020-12-29 22:07:42 +0000

    Puzzle 6 and clean up.

---
+++ AdventOfCode/Puzzle6/Part1/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Puzzle6.Part1
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var groups = string.Join(string.Empty, File.ReadAllLines(@"Puzzle6\Part1\Input.txt")
+                .Select(l => l == string.Empty ? "-" : l))
+                .Split('-');
+
+            var yesAnswerCount = 0;
+
+            foreach (var group in groups)
+            {
+                var uniqueQuestionsAnsweredInGroup = new List<char>();
+
+                foreach (var question in group)
+                {
+                    if (!uniqueQuestionsAnsweredInGroup.Contains(question))
+                    {
+                        uniqueQuestionsAnsweredInGroup.Add(question);
+                        yesAnswerCount++;
+
+                    }
+                }
+            }
+
+            Console.WriteLine(yesAnswerCount);
+        }
+    }
+}
---
+++ AdventOfCode/Puzzle6/Part2/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Puzzle6.Part2
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var groups = string.Join(",", File.ReadAllLines(@"Puzzle6\Part2\Input.txt")
+                .Select(l => l == string.Empty ? "-" : l))
+                .Split('-');
+
+            var everyoneAnsweredYesToQuestionsCount = 0;
+
+            foreach (var group in groups)
+            {
+                var answersByPerson = group.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                var questionCounts = new Dictionary<char, int>();
+
+                foreach (var answerByPerson in answersByPerson)
+                {
+                    foreach (var question in answerByPerson)
+                    {
+                        questionCounts[question] = questionCounts.ContainsKey(question) ?
+                            questionCounts[question] + 1 : 1;
+                    }
+                }
+
+                foreach (var questionKey in questionCounts.Keys)
+                {
+                    var countOfYesAnswersToQuestionInGroup = questionCounts[questionKey];
+
+                    if (countOfYesAnswersToQuestionInGroup == answersByPerson.Length)
+                    {
+                        everyoneAnsweredYesToQuestionsCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine(everyoneAnsweredYesToQuestionsCount);
+        }
+    }
+}

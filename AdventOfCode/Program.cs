f2bc7de7e12d78feffac1c428198f89d06d1fb20
Ravinder Singh <rsingh@ravi-singh.com>
2020-12-29 23:14:26 +0000

    Puzzle 24 and cleanup.

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            ISolution solution = new Puzzle23.Part2.Solution();
+            ISolution solution = new Puzzle24.Part2.Solution();
             solution.Run();
 
             Console.WriteLine("Press any key to quit");

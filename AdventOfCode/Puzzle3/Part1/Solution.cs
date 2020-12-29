d7fdb47151a69d9bd92fdb547cbc7e8218ff0ef5
Ravinder Singh <rsingh@ravi-singh.com>
2020-12-29 21:58:12 +0000

    Puzzle 3 and cleanup.

---
+++ AdventOfCode/Puzzle3/Part1/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode.Puzzle3.Part1
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var grid = File.ReadAllLines(@"Puzzle3\Part1\Input.txt");
+            var colIndex = 3;
+            var treesEncountered = 0;
+
+            for (var rowIndex = 1; rowIndex < grid.Length; rowIndex++)
+            {
+                var square = grid[rowIndex][colIndex];
+
+                if (square == '#')
+                {
+                    treesEncountered++;
+                }
+
+                colIndex = (colIndex + 3) % grid[0].Length;
+            }
+
+            Console.WriteLine(treesEncountered);
+        }
+    }
+}

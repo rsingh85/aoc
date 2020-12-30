f5cf7b9f46a90defd1ea21bd6fdc707bcd977ada
Ravinder Singh <rsingh@ravi-singh.com>
2020-12-30 18:33:29 +0000

    Puzzle 14 Part 1 cleanup.

--- AdventOfCode/Puzzle14/Part1/Solution.cs
+++ AdventOfCode/Puzzle14/Part1/Solution.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Linq;
 
 namespace AdventOfCode.Puzzle14.Part1
@@ -8,7 +10,45 @@ namespace AdventOfCode.Puzzle14.Part1
     {
         public void Run()
         {
-            throw new NotImplementedException();
+            var lines = File.ReadAllLines(@"Puzzle14\Part1\Input.txt");
+            var memoryAddresses = new Dictionary<string, long>();
+            var currentMask = string.Empty;
+
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("mask"))
+                {
+                    currentMask = Regex.Match(line, @"[X01]+$").Value;
+                }
+                else
+                {
+                    var memoryAddress = Regex.Match(line, @"^mem\[(\d+)\]").Groups[1].Value;
+                    var value = long.Parse(Regex.Match(line, @"\d+$").Value);
+                    var binaryValue = Convert.ToString(value, 2).PadLeft(currentMask.Length, '0');
+                    var result = string.Empty;
+            
+                    for (var i = 0; i < currentMask.Length; i++)
+                    {
+                        var currentMaskChar = currentMask[i];
+                    
+                        switch (currentMaskChar)
+                        {
+                            case '0':
+                                result += "0";
+                                break;
+                            case '1':
+                                result += "1";
+                                break;
+                            default:
+                                result += binaryValue[i];
+                                break;
+                        }
+                    }
+                    memoryAddresses[memoryAddress] = Convert.ToInt64(result, 2);
+                }
+            }
+
+            Console.WriteLine(memoryAddresses.Values.Sum());
         }
     }
 }

75e3cd92d266158ce3189bd146701952cfe6e373
Ravinder Singh <ravi.singh.uk85@gmail.com>
2021-11-27 21:36:51 +0000

    Puzzle 25

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            ISolution solution = new Y2020.Puzzle24.Part2.Solution();
+            ISolution solution = new Y2020.Puzzle25.Part1.Solution();
             solution.Run();
 
             Console.WriteLine("Press any key to quit");

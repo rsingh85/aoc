4441bc3931b20ad4278af714a287f1231d7bd282
Ravi Singh <ravi.singh.uk85@gmail.com>
2021-12-01 18:25:00 +0000

    Year 2021, day 1

---
+++ AdventOfCode/Y2021/Puzzle1/Part1/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Y2021.Puzzle1.Part1
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var depths = File.ReadAllLines(Helper.GetInputFilePath(this)).Select(int.Parse).ToArray();
+
+            var increases = depths
+                .Select((d, i) =>
+                    (i < depths.Length - 1) ?
+                        ((d < depths[i + 1]) ? 1 : 0) : 0)
+                .Sum();
+
+            Console.WriteLine(increases);
+        }
+    }
+}
---
+++ AdventOfCode/Y2021/Puzzle1/Part2/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Y2021.Puzzle1.Part2
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var depths = File.ReadAllLines(Helper.GetInputFilePath(this)).Select(int.Parse).ToArray();
+
+            var increases = depths
+                .Select((d, i) =>
+                    (i > 0 && i < depths.Length - 2) ?
+                        ((d + depths[i + 1] + depths[i + 2] > depths[i - 1] + d + depths[i + 1]) ? 1 : 0) : 0)
+                .Sum();
+
+            Console.WriteLine(increases);
+        }
+    }
+}

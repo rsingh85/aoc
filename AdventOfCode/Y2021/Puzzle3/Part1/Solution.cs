5ab4534114de48c1aff949d10d159c24cdc5e57c
Ravi Singh <ravi.singh.uk85@gmail.com>
2021-12-03 10:16:53 +0000

    Year 2021, day 3

---
+++ AdventOfCode/Y2021/Puzzle3/Part1/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Y2021.Puzzle3.Part1
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var binaries = File.ReadAllLines(Helper.GetInputFilePath(this));
+            string gRate = string.Empty, eRate = string.Empty;
+
+            for (var i = 0; i < binaries.First().Length; i++)
+            {
+                var zeroes = binaries.Select(b => b[i]).Count(b => b == '0');
+                var ones = binaries.Select(b => b[i]).Count(b => b == '1');
+
+                gRate += (zeroes > ones) ? "0" : "1";
+                eRate += (zeroes > ones) ? "1" : "0";
+            }
+
+            var powerConsumption = Convert.ToInt32(gRate, 2) * Convert.ToInt32(eRate, 2);
+
+            Console.WriteLine(powerConsumption);
+        }
+    }
+}
---
+++ AdventOfCode/Y2021/Puzzle3/Part2/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Y2021.Puzzle3.Part2
+{
+    public class Solution : ISolution
+    {
+        private enum Rating { OxygenGenerator, CO2Scrubber }
+
+        public void Run()
+        {
+            var binaries = File.ReadAllLines(Helper.GetInputFilePath(this));
+
+            var lifeSupportRating =
+                FindRating(binaries, 0, Rating.OxygenGenerator) *
+                FindRating(binaries, 0, Rating.CO2Scrubber);
+
+            Console.WriteLine(lifeSupportRating);
+        }
+
+        private int FindRating(IEnumerable<string> candidates, int index, Rating rating)
+        {
+            if (candidates.Count() > 1)
+            {
+                var zeroes = candidates.Select(b => b[index]).Count(b => b == '0');
+                var ones = candidates.Select(b => b[index]).Count(b => b == '1');
+
+                if (zeroes > ones)
+                {
+                    return FindRating(candidates.Where(b => (rating == Rating.OxygenGenerator) ? b[index] == '0' : b[index] == '1').ToList(), ++index, rating);
+                }
+                else if (ones > zeroes)
+                {
+                    return FindRating(candidates.Where(b => (rating == Rating.OxygenGenerator) ? b[index] == '1' : b[index] == '0').ToList(), ++index, rating);
+                }
+                else
+                {
+                    return FindRating(candidates.Where(b => (rating == Rating.OxygenGenerator) ? b[index] == '1' : b[index] == '0').ToList(), ++index, rating);
+                }
+            }
+
+            return Convert.ToInt32(candidates.First(), 2);
+        }
+    }
+}

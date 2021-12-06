4cc750dea14e336d1872a1b89cbfb07389fb0c1e
Ravi Singh <ravi.singh.uk85@gmail.com>
2021-12-06 16:27:28 +0000

    Year 2021, day 6

---
+++ AdventOfCode/Y2021/Puzzle6/Part1/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Y2021.Puzzle6.Part1
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            const int MaxDays = 80;
+
+            var timers = File.ReadAllLines(Helper.GetInputFilePath(this)).First().Split(",").Select(int.Parse).ToList();
+
+            for (var day = 1; day <= MaxDays; day++)
+            {
+                var timersCount = timers.Count();
+
+                for (var i = 0; i < timersCount; i++)
+                {
+                    var timer = timers[i];
+
+                    if (timer == 0)
+                    {
+                        timers[i] = 6;
+                        timers.Add(8);
+                        continue;
+                    }
+
+                    timers[i] = --timer;
+                }
+            }
+
+            Console.WriteLine(timers.Count());
+        }
+    }
+}
---
+++ AdventOfCode/Y2021/Puzzle6/Part2/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace AdventOfCode.Y2021.Puzzle6.Part2
+{
+    public class Solution : ISolution
+    {
+        // Optimised solution as my Puzzle6.Part1 implementation would take too long as MaxDays is increased
+        public void Run()
+        {
+            const int MaxDays = 256;
+            const int MaxTimer = 8;
+
+            var timers = File.ReadAllLines(Helper.GetInputFilePath(this)).First().Split(",").Select(int.Parse);
+            var timerCounts = new long[MaxTimer + 1];
+
+            foreach (var timer in timers)
+            {
+                timerCounts[timer]++;
+            }
+
+            for (var day = 1; day <= MaxDays; day++)
+            {
+                var zeroTimersCount = timerCounts[0];
+
+                for (var timer = 0; timer <= MaxTimer; timer++)
+                {
+                    if (timer == 8)
+                    {
+                        timerCounts[timer] = zeroTimersCount;
+                        continue;
+                    }
+
+                    timerCounts[timer] = timerCounts[timer + 1];
+
+                    if (timer == 6)
+                    {
+                        timerCounts[timer] += zeroTimersCount;
+                    }
+                }
+            }
+
+            Console.WriteLine(timerCounts.Sum());
+        }
+    }
+}

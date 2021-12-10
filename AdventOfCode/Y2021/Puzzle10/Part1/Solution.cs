99eafb7a7b35724a1245d5bccf0d814034473254
Ravi Singh <ravi.singh.uk85@gmail.com>
2021-12-10 13:13:44 +0000

    Year 2021, day 10

---
+++ AdventOfCode/Y2021/Puzzle10/Part1/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode.Y2021.Puzzle10.Part1
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var lines = File.ReadAllLines(Helper.GetInputFilePath(this));
+            var illegalChars = new List<char>();
+            var openChars = new List<char> { '(', '[', '{', '<' };
+
+            foreach (var line in lines)
+            {
+                var stack = new Stack<char>();
+                var syntaxErrorOnLine = false;
+
+                for (var i = 0; i < line.Length && !syntaxErrorOnLine; i++)
+                {
+                    var character = line[i];
+
+                    if (openChars.Contains(character))
+                    {
+                        stack.Push(character);
+                    }
+                    else
+                    {
+                        var top = stack.Pop();
+
+                        if (top == '(' && character != ')' ||
+                            top == '[' && character != ']' ||
+                            top == '{' && character != '}' ||
+                            top == '<' && character != '>')
+                        {
+                            syntaxErrorOnLine = true;
+                            illegalChars.Add(character);
+                        }
+                    }
+                }
+            }
+
+            var a = illegalChars.Count(c => c == ')') * 3;
+            var b = illegalChars.Count(c => c == ']') * 57;
+            var c = illegalChars.Count(c => c == '}') * 1197;
+            var d = illegalChars.Count(c => c == '>') * 25137;
+
+            Console.WriteLine(a + b + c + d);
+        }
+    }
+}
---
+++ AdventOfCode/Y2021/Puzzle10/Part2/Solution.cs
@@ -0,0 +1,107 @@
+﻿using System.Text;
+
+namespace AdventOfCode.Y2021.Puzzle10.Part2
+{
+    public class Solution : ISolution
+    {
+        private List<char> _openChars = new List<char> { '(', '[', '{', '<' };
+
+        public void Run()
+        {
+            var lines = File.ReadAllLines(Helper.GetInputFilePath(this));
+            var completionStrings = GetCompletionLines(lines.Where(l => !HasSyntaxError(l)));
+            var scores = new List<long>();
+
+            foreach (var completionString in completionStrings)
+            {
+                long score = 0;
+
+                foreach (var character in completionString)
+                {
+                    score *= 5;
+
+                    switch (character)
+                    {
+                        case ')': score += 1; break;
+                        case ']': score += 2; break;
+                        case '}': score += 3; break;
+                        case '>': score += 4; break;
+                    }
+                }
+
+                scores.Add(score);
+            }
+
+            scores.Sort();
+
+            Console.WriteLine(scores[scores.Count() / 2]);
+        }
+
+        private bool HasSyntaxError(string line)
+        {
+            var stack = new Stack<char>();
+            var syntaxError = false;
+
+            for (var i = 0; i < line.Length && !syntaxError; i++)
+            {
+                var character = line[i];
+
+                if (_openChars.Contains(character))
+                {
+                    stack.Push(character);
+                }
+                else
+                {
+                    var top = stack.Pop();
+
+                    if (top == '(' && character != ')' ||
+                        top == '[' && character != ']' ||
+                        top == '{' && character != '}' ||
+                        top == '<' && character != '>')
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private IEnumerable<string> GetCompletionLines(IEnumerable<string> incompleteLines)
+        {
+            var stack = new Stack<char>();
+
+            var reverseOpenCharMap = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '[', ']' },
+                { '{', '}' },
+                { '<', '>' }
+            };
+
+            foreach (var line in incompleteLines)
+            {
+                foreach (var character in line)
+                {
+                    if (_openChars.Contains(character))
+                    {
+                        stack.Push(character);
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+
+                var completionString = new StringBuilder();
+
+                while (stack.Count > 0)
+                {
+                    completionString.Append(reverseOpenCharMap[stack.Pop()]);
+                }
+
+                yield return completionString.ToString();
+            }
+        }
+    }
+}

153ae634c39ddcc9bde2dab4803e7d88a7000b0b
Ravinder Singh <rsingh@ravi-singh.com>
2021-12-18 11:19:37 +0000

    Year 2021, day 17

---
+++ AdventOfCode/Y2021/Puzzle17/Part1/Solution.cs
@@ -0,0 +1,72 @@
+﻿namespace AdventOfCode.Y2021.Puzzle17.Part1
+{
+    public class Solution : ISolution
+    {
+        private int _minX = 14, _maxX = 50, _minY = -267, _maxY = -225;
+
+        public void Run()
+        {
+            var highestY = 0;
+
+            for (var xv = 1; xv <= _maxX; xv++)
+            {
+                for (var yv = _maxY; yv <= 300; yv++)
+                {
+                    var result = HitsTarget(xv,yv);
+
+                    if (result.TargetHit)
+                    {
+                        highestY = Math.Max(highestY, result.HighestY);
+                    }
+                }
+            }
+
+            Console.WriteLine(highestY);
+        }
+
+        private (bool TargetHit, int HighestY) HitsTarget(int xVelocity, int yVelocity)
+        {
+            int x = 0, y = 0, highestY = 0;
+            var stop = false;
+
+            while (!stop)
+            {
+                x += xVelocity;
+                y += yVelocity;
+
+                highestY = Math.Max(highestY, y);
+
+                if (IsInTargetArea(x, y))
+                {
+                    return (true, highestY);
+                }
+                else if (MissesTargetArea(x, y))
+                {
+                    return (false, 0);
+                }
+
+                if (!stop)
+                {
+                    if (xVelocity > 0)
+                        xVelocity--;
+                    else if (xVelocity < 0)
+                        xVelocity++;
+
+                    yVelocity--;
+                }
+            }
+
+            return (false, 0);
+        }
+
+        private bool IsInTargetArea(int x, int y)
+        {
+            return x >= _minX && x <= _maxX && y >= _minY && y <= _maxY;
+        }
+
+        private bool MissesTargetArea(int x, int y)
+        {
+            return x > _maxX || y < _maxY;
+        }
+    }
+}

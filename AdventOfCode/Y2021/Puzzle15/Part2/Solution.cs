ba0938b147be2d1304c2889baed5cbe1248ec7d6
Ravinder Singh <rsingh@ravi-singh.com>
2021-12-20 18:14:10 +0000

    Year 2021, day 15

--- AdventOfCode/Y2021/Puzzle15/Part2/Solution.cs
+++ AdventOfCode/Y2021/Puzzle15/Part2/Solution.cs
@@ -1,9 +1,7 @@
 ﻿namespace AdventOfCode.Y2021.Puzzle15.Part2
 {
     /// <summary>
-    /// Re-learned and used Dijkstra's Shorting Path algorithm.
-    /// Useful video with pseudo code: 
-    /// https://www.youtube.com/watch?v=pVfj6mxhdMw&ab_channel=ComputerScience 
+    /// Requires optimising, currently very slow but gets the answer
     /// </summary>
     public class Solution : ISolution
     {
@@ -81,60 +79,61 @@
 
         private List<VertexInfo> ComputePaths()
         {
-            var visited = new HashSet<Vertex>();
-            var unvisited = new HashSet<Vertex>();
             var vertexInfos = new List<VertexInfo>();
 
             for (var r = 0; r < _gridMaxR; r++)
             {
                 for (var c = 0; c < _gridMaxC; c++)
                 {
-                    var vertex = new Vertex(r, c);
-
-                    unvisited.Add(vertex);
                     vertexInfos.Add(new VertexInfo
                     {
-                        Vertex = vertex,
+                        Vertex = new Vertex(r, c),
+                        Risk = _grid[r, c],
                         ShortestRiskDistanceFromStart = r == 0 && c == 0 ? 0 : int.MaxValue
                     });
                 }
             }
 
-            while (unvisited.Any())
-            {
-                var currentVertexInfo = 
-                    vertexInfos
-                        .Where(i => unvisited.Contains(i.Vertex))
-                        .OrderBy(i => i.ShortestRiskDistanceFromStart).First();
+            var visited = 1;
 
-                var currentVertex = currentVertexInfo.Vertex;
+            while (vertexInfos.Any(i => !i.Visited))
+            {
+                var unvisitedVertexInfos =
+                    vertexInfos.Where(i => !i.Visited).ToList();
+
+                var currentVertexInfo =
+                    unvisitedVertexInfos
+                        .OrderBy(i => i.ShortestRiskDistanceFromStart)
+                        .First();
 
                 var unvisitedNeighbourVertices =
-                    unvisited
+                    unvisitedVertexInfos
                         .Where(uv =>
-                            (uv.R == currentVertex.R - 1 && uv.C == currentVertex.C) || // top
-                            (uv.R == currentVertex.R && uv.C == currentVertex.C + 1) || // right
-                            (uv.R == currentVertex.R + 1 && uv.C == currentVertex.C) || // bottom
-                            (uv.R == currentVertex.R && uv.C == currentVertex.C - 1)    // left
-                        );
+                            (uv.Vertex.R == currentVertexInfo.Vertex.R - 1 && uv.Vertex.C == currentVertexInfo.Vertex.C) || // top
+                            (uv.Vertex.R == currentVertexInfo.Vertex.R && uv.Vertex.C == currentVertexInfo.Vertex.C + 1) || // right
+                            (uv.Vertex.R == currentVertexInfo.Vertex.R + 1 && uv.Vertex.C == currentVertexInfo.Vertex.C) || // bottom
+                            (uv.Vertex.R == currentVertexInfo.Vertex.R && uv.Vertex.C == currentVertexInfo.Vertex.C - 1)    // left
+                        ).ToList();
 
-                foreach (var vertex in unvisitedNeighbourVertices)
+                foreach (var vertexInfo in unvisitedNeighbourVertices)
                 {
                     var distanceFromStartVertex =
-                        currentVertexInfo.ShortestRiskDistanceFromStart + 
-                            _grid[vertex.R, vertex.C];
+                        currentVertexInfo.ShortestRiskDistanceFromStart + vertexInfo.Risk;
 
-                    var unvisitedNeighbourVertexInfo = vertexInfos.Single(vi => vi.Vertex == vertex);
-
-                    if (distanceFromStartVertex < unvisitedNeighbourVertexInfo.ShortestRiskDistanceFromStart)
+                    if (distanceFromStartVertex < vertexInfo.ShortestRiskDistanceFromStart)
                     {
-                        unvisitedNeighbourVertexInfo.ShortestRiskDistanceFromStart = distanceFromStartVertex;
-                        unvisitedNeighbourVertexInfo.PreviousVertex = currentVertex;
+                        vertexInfo.ShortestRiskDistanceFromStart = distanceFromStartVertex;
+                        vertexInfo.PreviousVertex = currentVertexInfo.Vertex;
                     }
                 }
 
-                visited.Add(currentVertex);
-                unvisited.Remove(currentVertex);
+                if (visited % 1000 == 0)
+                {
+                    Console.WriteLine("Visited {0} vertices", visited);
+                }
+
+                visited++;
+                currentVertexInfo.Visited = true;
             }
 
             return vertexInfos;
@@ -149,6 +148,8 @@
     public class VertexInfo
     {
         public Vertex Vertex { get; set; }
+        public int Risk { get; set; }
+        public bool Visited { get; set; }
         public int ShortestRiskDistanceFromStart { get; set; }
         public Vertex? PreviousVertex { get; set; }
         public override string ToString() => Vertex.ToString();

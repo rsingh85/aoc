18ef14bd43db05c68577a99afb63a2a397dc0e1d
Ravinder Singh <rsingh@ravi-singh.com>
2022-12-01 20:22:08 +0000

    Variable rename

--- 2022/Puzzle01/Part1/index.js
+++ 2022/Puzzle01/Part1/index.js
@@ -2,7 +2,7 @@ const readFileSyncIntoString = require("../../Core/ReadFileSyncIntoString");
 const input = readFileSyncIntoString("./data.txt").split("\n\n");
 
 const calories = input
-  .map((e) => e.split("\n").map(Number))
+  .map((c) => c.split("\n").map(Number))
   .map((arr) => arr.reduce((acc, curr) => acc + curr, 0));
 
 console.log(Math.max(...calories));

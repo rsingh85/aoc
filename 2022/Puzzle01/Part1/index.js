c702178d6225687f6377421b9c818ca3b4868c62
Ravi Singh <ravi.singh.uk85@gmail.com>
2022-12-01 15:53:06 +0000

    Fix

--- 2022/Puzzle01/Part1/index.js
+++ 2022/Puzzle01/Part1/index.js
@@ -7,14 +7,16 @@ let currentElfCalories = 0;
 for (let i = 0; i < input.length; i++) {
     const calories = input[i];
 
+    currentElfCalories += 
+        calories === "" ? 
+            0 : Number(calories);
+
     if (calories === "" ||  i == input.length - 1) {
         currentMaxElfCalories = 
             Math.max(currentElfCalories, currentMaxElfCalories)
 
         currentElfCalories = 0;
     }
-
-    currentElfCalories += Number(calories);
 }
 
 console.log(currentMaxElfCalories);

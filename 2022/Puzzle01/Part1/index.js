cd040de979fe0bc1c004d433e889bc570e152874
Ravinder Singh <rsingh@ravi-singh.com>
2022-12-01 23:17:26 +0000

    init npm and add eslint config

--- 2022/Puzzle01/Part1/index.js
+++ 2022/Puzzle01/Part1/index.js
@@ -1,8 +1,8 @@
-const readFileSyncIntoString = require("../../Core/ReadFileSyncIntoString");
-const input = readFileSyncIntoString("./data.txt").split("\n\n");
+const readFileSyncIntoString = require('../../Core/ReadFileSyncIntoString');
+const input = readFileSyncIntoString('./data.txt').split('\n\n');
 
 const calories = input
-  .map((c) => c.split("\n").map(Number))
-  .map((arr) => arr.reduce((acc, curr) => acc + curr));
+    .map((c) => c.split('\n').map(Number))
+    .map((arr) => arr.reduce((acc, curr) => acc + curr));
 
 console.log(Math.max(...calories));
--- 2022/Puzzle01/Part2/index.js
+++ 2022/Puzzle01/Part2/index.js
@@ -1,11 +1,12 @@
-const readFileSyncIntoString = require("../../Core/ReadFileSyncIntoString");
-const input = readFileSyncIntoString("./data.txt").split("\n\n");
+const readFileSyncIntoString = require('../../Core/ReadFileSyncIntoString');
+const input = readFileSyncIntoString('./data.txt').split('\n\n');
 
 const calories = input
-  .map((c) => c.split("\n").map(Number))
-  .map((arr) => arr.reduce((acc, curr) => acc + curr));
+    .map((c) => c.split('\n').map(Number))
+    .map((arr) => arr.reduce((acc, curr) => acc + curr));
 
-calories.sort(function (a, b) {
+calories.sort(function(a, b) {
   return b - a;
 });
+
 console.log(calories[0] + calories[1] + calories[2]);

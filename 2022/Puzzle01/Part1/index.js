8b715d54b97af64a9190a5da59894d70e2276ac8
Ravi Singh <ravi.singh.uk85@gmail.com>
2022-12-01 13:28:34 +0000

    Use Math.max

--- 2022/Puzzle01/Part1/index.js
+++ 2022/Puzzle01/Part1/index.js
@@ -1,14 +1,15 @@
 const readFileIntoArray = require("../../Core/ReadFileIntoArray");
 const input = readFileIntoArray("./data.txt");
 
-let max = 0;
+let currentMaxElfCalories = 0;
 let currentElfCalories = 0;
 
 for (let i = 0; i < input.length; i++) {
     const calories = input[i];
 
     if (calories === "" ||  i == input.length - 1) {
-        max = (currentElfCalories > max) ? currentElfCalories : max;
+        currentMaxElfCalories = 
+            Math.max(currentElfCalories, currentMaxElfCalories)
 
         currentElfCalories = 0;
     }
@@ -16,4 +17,4 @@ for (let i = 0; i < input.length; i++) {
     currentElfCalories += Number(calories);
 }
 
-console.log(max);
+console.log(currentMaxElfCalories);

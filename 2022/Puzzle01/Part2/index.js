75475a26d70633bfa7d21a490e9afb8c390ead4a
Ravi Singh <ravi.singh.uk85@gmail.com>
2022-12-01 16:10:17 +0000

    2022, Puzzle 1, Part 2

--- 2022/Puzzle01/Part2/index.js
+++ 2022/Puzzle01/Part2/index.js
@@ -1,22 +1,20 @@
 const readFileIntoArray = require("../../Core/ReadFileIntoArray");
 const input = readFileIntoArray("./data.txt");
 
-let currentMaxElfCalories = 0;
 let currentElfCalories = 0;
+let elfCalories = [];
 
 for (let i = 0; i < input.length; i++) {
     const calories = input[i];
 
     currentElfCalories += 
-        calories === "" ? 
-            0 : Number(calories);
+        calories === "" ? 0 : Number(calories);
 
     if (calories === "" ||  i == input.length - 1) {
-        currentMaxElfCalories = 
-            Math.max(currentElfCalories, currentMaxElfCalories);
-
+        elfCalories.push(currentElfCalories);
         currentElfCalories = 0;
     }
 }
 
-console.log(currentMaxElfCalories);
+elfCalories.sort(function(a, b) { return b - a } );
+console.log(elfCalories[0] + elfCalories[1] + elfCalories[2]);

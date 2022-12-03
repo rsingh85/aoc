29216473f4ffde8b66ff12d3aa000ee1eb71d0bb
Ravinder Singh <rsingh@ravi-singh.com>
2022-12-03 11:30:25 +0000

    Using map reduce

--- 2022/Puzzle03/Part1/index.js
+++ 2022/Puzzle03/Part1/index.js
@@ -2,22 +2,23 @@ const _ = require('underscore');
 const readFileSyncIntoArray = require('../../Core/ReadFileSyncIntoArray');
 const rucksacks = readFileSyncIntoArray('./data.txt');
 
+const getCommon = function(rucksack) {
+  return _.intersection(
+      [...rucksack.substring(0, rucksack.length / 2)],
+      [...rucksack.substring(rucksack.length / 2, rucksack.length)],
+  )[0];
+};
+
 const getPriority = function(item) {
   const ascii = item.charCodeAt(0);
   return item === item.toUpperCase() ?
      ascii - 65 + 27 : ascii - 97 + 1;
 };
 
-let sum = 0;
-
-for (const rucksack of rucksacks) {
-  const common =
-    _.intersection(
-        [...rucksack.substring(0, rucksack.length / 2)],
-        [...rucksack.substring(rucksack.length / 2, rucksack.length)],
-    )[0];
-
-  sum += getPriority(common);
-}
+const sum =
+  rucksacks
+      .map((r) => getCommon(r))
+      .map((c) => getPriority(c))
+      .reduce((acc, curr) => acc += curr);
 
 console.log(sum);

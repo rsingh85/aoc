147ac8f3909b985ce64b882ca60efd7123df2e74
Ravinder Singh <rsingh@ravi-singh.com>
2022-12-06 18:43:02 +0000

    Puzzle 6

---
+++ 2022/Puzzle06/Part2/index.js
@@ -0,0 +1,11 @@
+const _ = require('underscore');
+const readFileSyncIntoString = require('../../Core/ReadFileSyncIntoString');
+const data = readFileSyncIntoString('./data.txt');
+const scanLength = 14;
+
+for (let pointer = scanLength - 1; pointer < data.length; pointer++) {
+  if (_.uniq(data.slice(pointer - scanLength, pointer)).length === scanLength) {
+    console.log(pointer);
+    break;
+  }
+}

68e03ee6f05e96e4b7b83285e6b4ab783ae546fd
Ravinder Singh <rsingh@ravi-singh.com>
2022-12-06 21:22:13 +0000

    Fix pointer init

--- 2022/Puzzle06/Part2/index.js
+++ 2022/Puzzle06/Part2/index.js
@@ -3,7 +3,7 @@ const readFileSyncIntoString = require('../../Core/ReadFileSyncIntoString');
 const data = readFileSyncIntoString('./data.txt');
 const scanLength = 14;
 
-for (let pointer = scanLength - 1; pointer < data.length; pointer++) {
+for (let pointer = scanLength; pointer < data.length; pointer++) {
   if (_.uniq(data.slice(pointer - scanLength, pointer)).length === scanLength) {
     console.log(pointer);
     break;

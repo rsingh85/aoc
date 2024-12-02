641669eab37c8f55256e3c099ae39fc5943dad44
Ravinder Singh <rsingh@ravi-singh.com>
2024-12-02 15:35:02 +0000

    2024, Puzzle 2, Part 2

---
+++ 2020-2021-2024/AdventOfCode/Y2024/Puzzle2/Part2/Solution.cs
@@ -0,0 +1,76 @@
+﻿namespace AdventOfCode.Y2024.Puzzle2.Part2
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var reports = File.ReadAllLines(Helper.GetInputFilePath(this)).ToArray();
+            var safeReports = 0;
+
+            foreach (var report in reports)
+            {
+                var levels = report.Split(' ').Select(int.Parse).ToArray();
+                var permutations = GetLevelPermutations(levels);
+                
+                if (IsReportSafe(levels))
+                    safeReports++;
+                else
+                {
+                    foreach (var permutation in permutations)
+                    {
+                        if (IsReportSafe(permutation))
+                        {
+                            safeReports++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(safeReports);
+        }
+
+        private static bool IsReportSafe(int[] levels)
+        {
+            bool isIncreasingReport = levels[1] > levels[0];
+
+            for (var i = 0; i < levels.Length - 1; i++)
+            {
+                var diff = levels[i + 1] - levels[i];
+
+                if (isIncreasingReport && diff <= 0)
+                    return false;
+
+                if (!isIncreasingReport && diff >= 0)
+                    return false;
+
+                var absDiff = Math.Abs(diff);
+                if (!(absDiff >= 1 && absDiff <= 3))
+                    return false;
+            }
+
+            return true;
+        }
+
+        private static List<int[]> GetLevelPermutations(int[] levels)
+        {
+            var result = new List<int[]>();
+
+            for (int i = 0; i < levels.Length; i++)
+            {
+                var tempArray = new int[levels.Length - 1];
+                var index = 0;
+
+                for (var j = 0; j < levels.Length; j++)
+                {
+                    if (j != i)
+                        tempArray[index++] = levels[j];
+                }
+
+                result.Add(tempArray);
+            }
+
+            return result;
+        }
+    }
+}

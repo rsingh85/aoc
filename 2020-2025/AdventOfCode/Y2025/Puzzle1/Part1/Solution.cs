2e844c98188e106a3ba595ab3c8825d73284ec79
Ravinder Singh <Ravinder.Singh@maples.com>
2025-12-01 10:36:34 +0000

    Upgrade to .NET 10

---
+++ 2020-2025/AdventOfCode/Y2025/Puzzle1/Part1/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode.Y2025.Puzzle1.Part1
+{
+    public class Solution : ISolution
+    {
+        public void Run()
+        {
+            var lines = File.ReadAllLines(Helper.GetInputFilePath(this)).ToArray();
+            Console.WriteLine("Hello, world");
+        }
+    }
+}

e43712b0eb92ddf1d2464b3585fb3526506d2991
Ravinder Singh <Ravinder.Singh@maples.com>
2025-12-02 13:50:27 +0000

    2025, puzzle 2, part 2

---
+++ 2020-2025/AdventOfCode/Y2025/Puzzle2/Part2/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Y2025.Puzzle2.Part2
+{
+    public partial class Solution : ISolution
+    {
+        public void Run()
+        {
+            var ranges = File.ReadAllLines(Helper.GetInputFilePath(this))[0]
+                .Split(",")
+                .Select(r => {
+                    var parts = r.Split("-");
+                    return (start: long.Parse(parts[0]), end: long.Parse(parts[1]));
+                })
+                .ToList();
+
+            long sum = 0;
+
+            foreach (var range in ranges)
+            {
+                for (long number = range.start; number <= range.end; number++)
+                    if (IsInvalid(number))
+                        sum += number;
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static bool IsInvalid(long number) => RepeatingNumberPatternAtLeastTwice().IsMatch(number.ToString());
+
+        [GeneratedRegex(@"^(\d+)\1+$")]
+        private static partial Regex RepeatingNumberPatternAtLeastTwice();
+    }
+}

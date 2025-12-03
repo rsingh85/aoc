ca3c30d275581d01eb1d0483798dbd3733369fce
Ravinder Singh <Ravinder.Singh@maples.com>
2025-12-03 11:23:08 +0000

    2025, Puzzle 3, Part 1

---
+++ 2020-2025/AdventOfCode/Y2025/Puzzle3/Part1/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Y2025.Puzzle3.Part1
+{
+    public partial class Solution : ISolution
+    {
+        public void Run()
+        {
+            var banks = File.ReadAllLines(Helper.GetInputFilePath(this));
+            var largestJoltagesPerBank = new int[banks.Length];
+
+            for (var bankIndex = 0; bankIndex < banks.Length; bankIndex++)
+            {
+                var bank = banks[bankIndex];
+
+                for (var currentBatteryIndex = 0; currentBatteryIndex < bank.Length; currentBatteryIndex++)
+                {
+                    var currentBatteryJoltageChar = bank[currentBatteryIndex];
+
+                    for (var otherBatteryIndex = currentBatteryIndex + 1; otherBatteryIndex < bank.Length; otherBatteryIndex++)
+                    {
+                        var otherBatteryJoltageChar = bank[otherBatteryIndex];
+
+                        var totalJoltage = int.Parse($"{currentBatteryJoltageChar}{otherBatteryJoltageChar}");
+
+                        if (totalJoltage > largestJoltagesPerBank[bankIndex])
+                            largestJoltagesPerBank[bankIndex] = totalJoltage;
+                    }
+                }
+            }
+
+            Console.WriteLine(largestJoltagesPerBank.Sum());
+        }
+    }
+}

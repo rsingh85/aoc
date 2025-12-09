cb4bb1348a85449e740ce28fdb905f0cffae50f2
Ravi Singh <ravi.singh.uk85@gmail.com>
2025-12-09 12:40:35 +0000

    2025, Puzzle 5, Part 2

---
+++ 2020-2025/AdventOfCode/Y2025/Puzzle5/Part2/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode.Y2025.Puzzle5.Part2
+{
+    public partial class Solution : ISolution
+    {
+        public void Run()
+        {
+            var lines = File.ReadAllLines(Helper.GetInputFilePath(this));
+            var ranges = new List<(long Start, long End)>();
+
+            foreach (var line in lines)
+            {
+                if (line.Contains('-'))
+                {
+                    var split = line.Split('-');
+                    ranges.Add((long.Parse(split[0]), long.Parse(split[1])));
+                }
+            }
+
+            ranges.Sort((a, b) => a.Start.CompareTo(b.Start));
+
+            // Merge overlapping ranges
+            var mergedRanges = new List<(long Start, long End)>();
+
+            foreach (var range in ranges)
+            {
+                if (!mergedRanges.Any() || range.Start > mergedRanges[^1].End + 1)
+                    mergedRanges.Add(range);
+                else
+                {
+                    var lastRange = mergedRanges[^1];
+                    mergedRanges[^1] = (lastRange.Start, Math.Max(lastRange.End, range.End));
+                }
+            }
+
+            var totalFreshIngredients = 0L;
+            foreach (var mergedRange in mergedRanges)
+                totalFreshIngredients += mergedRange.End - mergedRange.Start + 1;
+
+            Console.WriteLine(totalFreshIngredients);
+        }
+    }
+}
